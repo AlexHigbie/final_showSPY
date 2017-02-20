39ee2e233c062845bf0d3fb0971abd7d88f243d7
Alexander Higbie <alexanderhigbie@Alexander-Higbies-MacBook-Pro.local>
2017-02-20 12:03:32 -0700

    api key

--- public/js/module.dashboard.js
+++ public/js/module.dashboard.js
@@ -1,6 +1,8 @@
 angular.module('module.dashboard', [])
     .controller('DashboardController', Dashboard);
 
+var api_key = process.env.GUIDEBOX_APIKEY
+
 
     Dashboard.$inject=['$http']
 
@@ -9,41 +11,34 @@ function Dashboard($http) {
 
     var dashCtl = this
 
-    function getNewShows(){
-      console.log("getting new shows")
-      $http.get('http://api-public.guidebox.com/v2/updates?object=show&type=new&time=1487177673&api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa')
-      .then(function(res, err){
-        console.log('new shows: ', res.data)
-      })
-    }
-
-    // function getNewShows () {
-    //   console.log('Making "get" request for new shows')
-    //   $http.get('http://api-public.guidebox.com/v2/updates?object=show&type=new&time=1487104021?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa')
+    // function getNewShows(){
+    //   console.log("getting new shows")
+    //   $http.get('http://api-public.guidebox.com/v2/updates?object=show&type=new&time=1487177673&api_key='+ api_key)
     //   .then(function(res, err){
-    //     console.log(res.data)
-    //     // dashCtl.newShows =
+    //     console.log('new shows: ', res.data)
     //   })
     // }
-    function test () {
-      console.log('Making "get" request for shows, TEST')
-      $http.get('http://api-public.guidebox.com/v2/shows?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa')
-      .then(function(res, err){
-        console.log(res.data)
-        console.log(res.data.results[0].title)
-        dashCtl.data = res.data
-        dashCtl.shows = [];
-        for(var i=0; i<dashCtl.data.total_returned; i++){
-            dashCtl.shows.push(dashCtl.data.results[i].title)
-        }
-        console.log(dashCtl.shows)
-      })
-    }
+
+
+    // function test () {
+    //   console.log('Making "get" request for shows, TEST')
+    //   $http.get('http://api-public.guidebox.com/v2/shows?api_key='+ api_key)
+    //   .then(function(res, err){
+    //     console.log(res.data)
+    //     console.log(res.data.results[0].title)
+    //     dashCtl.data = res.data
+    //     dashCtl.shows = [];
+    //     for(var i=0; i<dashCtl.data.total_returned; i++){
+    //         dashCtl.shows.push(dashCtl.data.results[i].title)
+    //     }
+    //     console.log(dashCtl.shows)
+    //   })
+    // }
 
     dashCtl.search = function(){
       var show = dashCtl.searchText
       console.log('you searched for ', show)
-      $http.get('http://api-public.guidebox.com/v2/search?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa&type=show&query='+ show)
+      $http.get('http://api-public.guidebox.com/v2/search?api_key='+api_key+ '&type=show&query='+ show)
       .then(function(res, err){
         console.log(res.data)
         dashCtl.showsResponse = res.data.results
@@ -53,12 +48,12 @@ function Dashboard($http) {
     dashCtl.showInfo = function(result) {
       console.log(result)
       // console.log(id)
-      $http.get('http://api-public.guidebox.com/v2/shows/' + result.id + '/available_content?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa')
+      $http.get('http://api-public.guidebox.com/v2/shows/' + result.id + '/available_content?api_key='+ api_key)
       .then(function(res, err){
         console.log(res.data)
         dashCtl.showInfoSources = res.data.results.web.episodes.all_sources
       })
-      $http.get('http://api-public.guidebox.com/v2/shows/'+ result.id +'?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa')
+      $http.get('http://api-public.guidebox.com/v2/shows/'+ result.id +'?api_key='+ api_key)
       .then(function(res, err){
         console.log(res.data)
         dashCtl.moreInfo = res.data
@@ -68,7 +63,7 @@ function Dashboard($http) {
     dashCtl.searchMovies = function(){
       var movie = dashCtl.searchMovieText
       console.log('you searched for ', movie)
-      $http.get('http://api-public.guidebox.com/v2/search?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa&type=movie&query='+ movie)
+      $http.get('http://api-public.guidebox.com/v2/search?api_key='+ api_key + '&type=movie&query='+ movie)
       .then(function(res, err){
         console.log(res.data)
         dashCtl.moviesResponse = res.data.results
@@ -83,7 +78,7 @@ function Dashboard($http) {
       //   console.log(res.data)
         // dashCtl.movieInfoSources = res.data.results.web.episodes.all_sources
       // })
-      $http.get('http://api-public.guidebox.com/v2/movies/'+ result.id +'?api_key=ae8273f03e1e2c7171d25f4fb27ad3b646063eaa')
+      $http.get('http://api-public.guidebox.com/v2/movies/'+ result.id +'?api_key='+ api_key)
       .then(function(res, err){
         console.log(res.data)
         dashCtl.movieInfo = res.data
